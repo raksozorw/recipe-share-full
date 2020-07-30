6302a27c015b6f2dc6c4778bc31e12f4feaa1fbb
Oskar Wroz <oskarwroz@gmail.com>
2020-07-30 14:30:01 -0700

    updated cred object

--- config/index.js
+++ config/index.js
@@ -7,8 +7,12 @@ const serviceKey = path.join(__dirname, "./keys.js");
 
 const { Storage } = Cloud;
 const storage = new Storage({
-  keyFilename: serviceKey,
+  // keyFilename: serviceKey,
   projectId: "streaming-278216",
+  credentials: {
+    client_email: process.env.CLIENT_EMAIL,
+    private_key: process.env.PRIVATE_KEY,
+  },
 });
 
 module.exports = storage;
