05b4fe16024149d53cdd8081cf4c859492a1e416
Oskar Wroz <oskarwroz@gmail.com>
2020-07-30 10:39:14 -0700

    google credentials moved to env variable

--- config/index.js
+++ config/index.js
@@ -1,10 +1,12 @@
 const Cloud = require("@google-cloud/storage");
 const path = require("path");
+require("dotenv").config();
+const serviceKey2 = process.env.GOOGLE_CREDENTIALS;
 const serviceKey = path.join(__dirname, "./keys.json");
 
 const { Storage } = Cloud;
 const storage = new Storage({
-  keyFilename: serviceKey,
+  keyFilename: serviceKey2,
   projectId: "streaming-278216",
 });
 
