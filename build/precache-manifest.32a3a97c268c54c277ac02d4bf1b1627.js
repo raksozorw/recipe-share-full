3eea28ff1325fa18fece42f1c2ae3a6aaf1c9b65
Oskar Wroz <oskarwroz@gmail.com>
2020-07-30 10:15:53 -0700

    build file removed from gitignore, now tracked

---
+++ build/precache-manifest.32a3a97c268c54c277ac02d4bf1b1627.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26df875089a7524d9e014ec636652a0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0102d65cd2a8d8048cd",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4cf7b79cc8098b9a8873",
+    "url": "/static/css/main.0e706d2e.chunk.css"
+  },
+  {
+    "revision": "d0102d65cd2a8d8048cd",
+    "url": "/static/js/2.778f62b0.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.778f62b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cf7b79cc8098b9a8873",
+    "url": "/static/js/main.94ad1dd0.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "0469911521f97dd6a457853e78cbcbe6",
+    "url": "/static/media/Home-thin.04699115.png"
+  },
+  {
+    "revision": "635d5bfe587dee4313649a6b4d21e62f",
+    "url": "/static/media/logo.635d5bfe.png"
+  },
+  {
+    "revision": "19c195b9fc93544fc25db8bfe637b55b",
+    "url": "/static/media/no-text-logo.19c195b9.png"
+  }
+]);
