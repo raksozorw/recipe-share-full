7dc6fb0ae4e0d61f09d5a6edcb96453d7788ef20
Oskar Wroz <oskarwroz@gmail.com>
2020-07-30 14:55:50 -0700

    still at it

--- config/index.js
+++ config/index.js
@@ -1,18 +1,16 @@
 const Cloud = require("@google-cloud/storage");
 const path = require("path");
 require("dotenv").config();
-const serviceKey2 = process.env.GOOGLE_CREDENTIALS;
+const serviceKey2 = process.env.GOOGLE_APPLICATION_CREDENTIALS;
+const credentials = process.env.GOOGLE_CREDENTIALS;
 
 const serviceKey = path.join(__dirname, "./keys.js");
 
 const { Storage } = Cloud;
 const storage = new Storage({
-  // keyFilename: serviceKey,
-  projectId: "streaming-278216",
-  credentials: {
-    client_email: process.env.CLIENT_EMAIL,
-    private_key: process.env.PRIVATE_KEY,
-  },
+  // keyFilename: serviceKey2,
+  // projectId: "streaming-278216",
+  credentials: credentials,
 });
 
 module.exports = storage;
